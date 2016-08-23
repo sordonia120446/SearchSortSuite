b125dc9c80dbf7d40e1a2c8a8724813c250138bb
sordonia120446 <samuel.ordonia@gmail.com>
2016-08-23 10:37:14 -0700

    Added in Hoare's partition algorithm for quicksort.  To call it, use quicksort2().

--- src/com/sordonia120446/Main.java
+++ src/com/sordonia120446/Main.java
@@ -5,29 +5,31 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[] myInts = {1,2,3,4,5};
-        int[] myInts2 = {3,2,12,7,5,10,17,4,16,18,225,0,-17};
-        int target = 1;
+        //int[] myInts2 = {3,2,12,7,5};
+        int[] myInts2 = {3,2,12,7,5,10,17,4,16,18,225,0,-17,2};
+        int target = 3;
 
-        // Binary search test
-        BinarySearch bs = new BinarySearch(myInts,target);
-        System.out.println("Binary search test...");
-        System.out.println(bs.binarySearch());
-        System.out.println("Done!\n\n");
+//        // Binary search test
+//        BinarySearch bs = new BinarySearch(myInts,target);
+//        System.out.println("Binary search test...");
+//        System.out.println(bs.binarySearch());
+//        System.out.println("Done!\n\n");
 
         // Quicksort test
         QuickSort qs = new QuickSort(myInts2);
-        int[] sortedArrByQS = qs.quickSort();
+        int[] sortedArrByQS = qs.quickSort2();
         System.out.println("\n\nQuicksort test...");
         for (int i:sortedArrByQS) {
             System.out.println(i);
         }
 
-        // Insertionsort test
-        InsertionSort is = new InsertionSort(myInts2);
-        int[] sortedArrByIS = is.insertionSort();
-        System.out.println("\n\nInsertionsort test...");
-        for (int i:sortedArrByIS) {
-            System.out.println(i);
-        }
-    }
-}
+//        // Insertionsort test
+//        InsertionSort is = new InsertionSort(myInts2);
+//        int[] sortedArrByIS = is.insertionSort();
+//        System.out.println("\n\nInsertionsort test...");
+//        for (int i:sortedArrByIS) {
+//            System.out.println(i);
+//        }
+
+    } // end method main
+} // end class Main
--- src/com/sordonia120446/QuickSort.java
+++ src/com/sordonia120446/QuickSort.java
@@ -25,6 +25,11 @@ public class QuickSort {
         return mArray;
     } // end method quickSort
 
+    public int[] quickSort2() {
+        qSort2(0,arrLength - 1);
+        return mArray;
+    } // end method quickSort
+
     private void qSort(int lo, int hi) {
         /*
         Splits up the array to be sorted by a partition index.  The sort begins from the last element
@@ -35,8 +40,17 @@ public class QuickSort {
             qSort(lo, pivotIndex - 1 );
             qSort(pivotIndex + 1, hi );
         }
-
     } // end method qSort
+
+    private void qSort2(int lo, int hi) {
+        // Same as qSort in architecture, except it implements a more complex version of the partition algorithm.
+        if ( (hi > lo) ) {
+            int pivotIndex = hoarePartition(lo, hi);
+            System.out.printf("The pivot index is now %d\n",pivotIndex);
+            qSort(lo, pivotIndex );
+            qSort(pivotIndex + 1, hi );
+        }
+    } // end method qSort2
 
     private int lomutoPartition(int lo, int hi) {
         /*
@@ -59,11 +73,44 @@ public class QuickSort {
         return cntr;
     } // end method partition
 
+    private int hoarePartition(int lo, int hi) {
+        /*
+        Implements Hoare's more complicated version of the quicksort partition.
+        There are two index counters that run from either end of the array until
+        crossing paths with each other.
+        In this case, the pivot element can either be the first or last element.
+        */
+        int pivotElement = mArray[lo];
+        int leftCntr = lo;
+        int rightCntr = hi;
+        while ( rightCntr > leftCntr ) {
+
+            while (mArray[leftCntr] < pivotElement) {
+                leftCntr ++;
+            } // end while
+
+            while (mArray[rightCntr] > pivotElement) {
+                rightCntr--;
+            } // end while
+
+            // Once the index cntrs cross each other, finish the loop and return the rightCntr index.
+            if (leftCntr >= rightCntr) {
+                return rightCntr;
+            } // end if
+
+            // Swap elements
+            mArray = swap(mArray,leftCntr,rightCntr);
+
+        } // end while
+        return rightCntr;
+    } // end method hoarePartition
+
     private int[] swap(int[] arr, int ind1, int ind2) {
         /*
         Swaps two elements in int[] array arr, given indices ind1 and ind2.
         Returns updated array with swapped elements.
         */
+        System.out.printf("Swapping %d and %d\n",arr[ind1],arr[ind2]);
         int tmp = arr[ind2];
         arr[ind2] = arr[ind1];
         arr[ind1] = tmp;
